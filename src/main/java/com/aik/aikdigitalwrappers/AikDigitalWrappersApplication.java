5bf5a92bb4ae159fb5941b8eace291805da0b04d
Wasi Abbas <169152339+RajaWasi110@users.noreply.github.com>
2025-10-02 11:31:59 +0500

    Initial commit

---
+++ src/main/java/com/aik/aikdigitalwrappers/AikDigitalWrappersApplication.java
@@ -0,0 +1,13 @@
+package com.aik.aikdigitalwrappers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikDigitalWrappersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikDigitalWrappersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aik/aikdigitalwrappers/AikDigitalWrappersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aik.aikdigitalwrappers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikDigitalWrappersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
